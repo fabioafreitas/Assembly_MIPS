94a0a74e49e4b6f043c81ef71202db28650945fa
fábio alves <fbio.alves095@gmail.com>
2021-10-12 14:28:48 -0300

    ...

---
+++ relembrando_mips/keyboard_arrows.asm
@@ -0,0 +1,18 @@
+.macro sleep(%miliseconds)
+	li $a0, %miliseconds
+	li $v0, 32
+	syscall
+.end_macro
+
+.text
+	loop:  
+  li   $v0, 12       
+  syscall            # Read Character
+
+  addiu $a0, $v0, 1  # $a0 gets the next char
+
+  li   $v0, 11       
+  syscall            # Write Character
+
+  b loop
+  nop
